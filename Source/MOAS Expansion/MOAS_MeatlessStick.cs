91adcb09686399f6a961d669b569b0ecd61ce595
chris-b-90 <kedjown@gmail.com>
2025-11-06 18:50:18 +0000

    Initial

---
+++ Source/MOAS Expansion/MOAS_MeatlessStick.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+
+namespace MOAS_Expansion
+{
+    public class IngredientValueGetter_MeatlessStick : IngredientValueGetter
+    {
+        public override float ValuePerUnitOf(ThingDef thingDef)
+        {
+            if (thingDef.IsNutritionGivingIngestible)
+            {
+                return thingDef.GetStatValueAbstract(StatDefOf.Nutrition);
+            }
+            if (thingDef.IsStuff)
+            {
+                // Allows using non-nutrition items in food recipes, i.e. the meatless stick
+                return thingDef.VolumePerUnit;
+            }
+            return 1f;
+        }
+
+        public override string BillRequirementsDescription(RecipeDef recipe, IngredientCount ingredient)
+        {
+            if (ingredient.IsFixedIngredient)
+            {
+                return ingredient.GetBaseCount() + "x " + ingredient.filter.Summary;
+            }
+            //return "BillRequiresNutrition".Translate(ingredient.GetBaseCount().ToString("F")) + " (" + ingredient.filter.Summary + ")";
+            return ingredient.GetBaseCount() + "x " + "BillNutrition".Translate() + " (" + ingredient.filter.Summary + ")";
+        }
+    }
+}
